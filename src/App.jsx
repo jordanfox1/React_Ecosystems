c9fd2ff013ad46ba9f5ebc7866222ee3477261cb
Jordan Fox <jordan.fox@canstar.com.au>
2022-07-29 22:31:25 +1000

    put app together

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { Fragment } from 'react'
+import TodoList from './todos/TodoList'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+      
   return (
-      <h1>Hello world</h1>
+    <Fragment>
+      <TodoList />  
+    </Fragment>    
   )
 }
 
