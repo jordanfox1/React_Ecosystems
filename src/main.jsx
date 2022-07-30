8d83ca9bacc4ee797e9685d8aeb57ae25a771df0
Jordan Fox <jordan.fox@canstar.com.au>
2022-07-30 11:47:13 +1000

    added persisting store data

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,18 @@ import App from './App'
 import './index.css'
 import { Provider } from 'react-redux'
 import { configureStore } from './store'
+import { persistStore } from 'redux-persist'
+import { PersistGate } from 'redux-persist/lib/integration/react'
+
+const store = configureStore()
+const persistor = persistStore(store)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={configureStore()} >
-      <App />
+    <Provider store={store} >
+
+      <PersistGate persistor={persistor} loading={<div>Loading...</div>}>
+        <App />
+      </PersistGate>
+
     </Provider>
-  </React.StrictMode>
 )
