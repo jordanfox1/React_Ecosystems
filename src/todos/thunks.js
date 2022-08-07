2d70935636e72f8b0be0d9c06bd1af3e59e70354
Jordan Fox <jordan.fox@canstar.com.au>
2022-08-07 10:39:13 +1000

    added create new todo thunk

--- src/todos/thunks.js
+++ src/todos/thunks.js
@@ -1,19 +1,39 @@
 // in redux a thunk is a function that returns another function which contains the logic to perform when its triggered
-import { loadTodosInProgress, loadTodosFailure, loadTodosSuccess} from "./actions"
+import { loadTodosInProgress, loadTodosFailure, loadTodosSuccess } from "./actions"
 
 export const loadTodos = () => async (dispatch, getState) => { //dispacth to and get state from the store
-    try{
+    try {
         dispatch(loadTodosInProgress())
         const response = await fetch('http://localhost:8080/todos')
         const todos = await response.json()
         dispatch(loadTodosSuccess(todos))
-    } catch(e) {
+    } catch (e) {
         dispatch(loadTodosFailure)
         dispatch(displayAlert(`failed to retrieve todos error: ${e}`))
     }
 
 }
 
+export function addTodoRequest(text) {
+    return async dispatch => {
+        try {
+            const body = JSON.stringify({ text })
+            const response = await fetch('http://localhost:8080/todos', {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                method: 'post',
+                body,
+            });
+            const todo = await response.json()
+            dispatch(createTodo(todo))
+        } catch (error) {
+            dispatch(displayAlert(error));
+            console.log('ERROR CREATING TODO, ', error?.message)
+        }
+
+    }
+}
 
 export const displayAlert = text => () => {
     alert(`you clikced on ${text}`)
